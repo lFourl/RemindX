dd2e710102c5480aa56254c2838b6b41b2820968
lFourl <darwest18@gmail.com>
2022-11-13 08:45:16 -0700

    Upload fully functioning to do list

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-0", name: "Test", completed: false },
+  { id: "todo-1", name: "These", completed: false },
+  { id: "todo-2", name: "Out", completed: false }
 ];
+
 
 ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
